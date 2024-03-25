c02fdd85bb76a5ae58b763f3a9f38df66f294648
daroOps <romerosd089@hotmail.com>
2024-03-24 21:42:09 -0500

    feat: :alembic: Add test component

---
+++ src/components/tcmp/Tcmp.jsx
@@ -0,0 +1,20 @@
+import './Tcmp.modules.css'
+
+const Tcmp = () => {
+
+    return (
+
+        <div className="dot-spinner">
+        <div className="dot-spinner__dot"></div>
+        <div className="dot-spinner__dot"></div>
+        <div className="dot-spinner__dot"></div>
+        <div className="dot-spinner__dot"></div>
+        <div className="dot-spinner__dot"></div>
+        <div className="dot-spinner__dot"></div>
+        <div className="dot-spinner__dot"></div>
+        <div className="dot-spinner__dot"></div>
+    </div>
+    );
+}
+
+export default Tcmp
