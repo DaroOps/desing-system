7291c93704dadd3c1312563e557d1c727d9f5e35
daroOps <romerosd089@hotmail.com>
2024-03-24 21:37:16 -0500

    feat: :construction: Add new imports and consts for routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import './App.css'
 import {Route, Routes, useLocation } from 'react-router-dom'
 
 //COMPONENTS
-import NavBar from './components/navBar/NavBar';
+import { NavBar } from './components';
 
 //VIEWS
-import Home from './views/Home';
+import { Home, Catalog } from './views'; 
+
+//ROUTES
+const routes = [
+  { path: '/home', label: 'Home' },
+  { path: '/catalog', label: 'Catalog' },
+];
 
 function App() {
 
@@ -13,9 +19,10 @@ function App() {
 
   return (
     <div className="App">
-        <NavBar className="App__nav"/>
+        <NavBar className="App__nav" routes={routes}/>
         <Routes>
             <Route path = '/home' Component={Home}/>
+            <Route path = '/catalog' Component={Catalog}/>
         </Routes>
     </div>
   )
